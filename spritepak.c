5058c376a8af87396b06a14b401f37256b3a748a
Pawel Pela <paulpela@gmail.com>
2016-01-27 01:02:26 +0100

    bugfixes, added messages in case of bad filenames

--- spritepak.c
+++ spritepak.c
@@ -1,3 +1,24 @@
+/*
+    spritepak - a compression utility for sprite/bitmap files
+
+    Copyright (C) 2016  Pawel Pela <paulpela@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
 #include<stdlib.h>
 #include<stdio.h>
 
@@ -6,7 +27,18 @@ int main(int argc, char *argv[]) {
     FILE *output;
 
     input = fopen(argv[1], "r");
+
+    if(!input) {
+        printf("Input file could not be opened. Exiting.\n");
+        exit (1);
+    }
+
     output = fopen(argv[2], "w");
+
+    if(!output) {
+        printf("Output file could not be opened. Exiting.\n");
+        exit (1);
+    }
 
     int counter = 0;
     int prevbyte = 0x00;
