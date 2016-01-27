8e208fb8a0719e815c70f2b4e004d731174400e7
Paweł Pela <paulpela@gmail.com>
2016-01-27 23:56:13 +0100

    first draft

--- spritepak.c
+++ spritepak.c
@@ -1,6 +1,29 @@
 #include<stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
+    FILE *input;
+    FILE *output;
 
+    input = fopen(argv[1], "r");
+    output = fopen(argv[2], "w");
+
+    int counter = 1;
+    int prevbyte = 0x00;
+    int byte = fgetc(input);
+
+    while(byte != EOF) {
+        if(byte != prevbyte) {
+            fputc(counter, output);
+            counter = 1;
+        } else {
+            counter++;
+        }
+
+        prevbyte = byte;
+        byte = fgetc(input);
+    }
+
+    fclose(input);
+    fclose(output);
     exit(0);
 }
